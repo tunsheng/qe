99440ca45b38ed371d6f344dfc98672fb1178f77
Tun Sheng Tan <tunsheng@cori03.nersc.gov>
2019-07-15 15:52:08 -0700

    Update copper example

--- cu-generate.sh
+++ cu-generate.sh
@@ -13,7 +13,7 @@
 #  3 - propagate wavefunction
 #  4 - structural optimization
 #  5 - MD run
-RUNS=3
+RUNS=2
 
 #################
 # COMMON INPUTS #
@@ -40,7 +40,7 @@ PREFIX=NULL         # Prefix for filenames
 
 # SYSTEM
 IBRAV=0    # Bravais-lattice index
-NAT=8      # Number of atoms in unit cell
+NAT=216      # Number of atoms in unit cell
 NTYP=1     # Number of types of atoms in unit cell
 ECUTWFC=35.0
 NBND='NULL'     # Number of electronics states/bands
@@ -70,6 +70,7 @@ do
 if [ $RUNS -eq 1 ];
 then #  1 - start with gram-schmidt
   MODE='from_scratch'
+  TITLE='Copper: Minimizing wavefunction [1 / 3 steps]'
   ISAVE=1
   IPRINT=1
   NDR=50
@@ -86,38 +87,41 @@ then #  1 - start with gram-schmidt
 elif [ $RUNS -eq 2 ];
 then #  2 - restart with orthogonal method
   MODE='restart'
+  TITLE='Copper: Minimizing wavefunction [2 / 3 steps]'
   ISAVE=10
   IPRINT=1
   NSTEP=100
   NDR=50
   NDW=51
-  TIMESTEP=5.0d0
-  ETOTCONVTHR='1.e-7'
+  TIMESTEP=10.0d0
+  #ETOTCONVTHR='1.e-7'
   ELECTRONDYNAMICS='sd'
-  EMASS=700.0
+  EMASS=1000.0
   EMCUTOFF=3.0
-  ORTHOMAX=100
+  ORTHOMAX=300
   IONDYNAMICS='none'
 elif [ $RUNS -eq 3 ];
 then # 3 - propagate ground state wavefunction
   MODE='restart'
+  TITLE='Copper: Minimizing wavefunction [3 / 3 steps]'
   ISAVE=10
   IPRINT=1
   NSTEP=100
-  TIMESTEP=2.d0
+  TIMESTEP=5.d0
   NDR=51
   NDW=52
-  ETOTCONVTHR='1.e-7'
+  #ETOTCONVTHR='1.e-7'
   ELECTRONDYNAMICS='damp'
   ELECTRONDAMPING=0.2
-  EMASS=700.0
+  EMASS=1000.0
   EMCUTOFF=3.0
-  ORTHOMAX=100
+  ORTHOMAX=300
   IONDYNAMICS='none'
 elif [ $RUNS -eq 4 ];
 then #  4 - structural optimization
   MODE='restart'
-  NDR=52
+  TITLE='Copper: Minimizing geometry [1 / 3 steps]'
+  NDR=54
   NDW=60
   IPRINT=1
   ISAVE=10
@@ -131,24 +135,24 @@ then #  4 - structural optimization
   ELECTRONDAMPING=0.001
   EMASS=500.0
   EMCUTOFF=3.0
-  ORTHOMAX=100
+  ORTHOMAX=300
   IONDYNAMICS='damp'
   IONVELOCITY='zero'
   IONDAMPING=0.02
 elif [ $RUNS -eq 5 ];
 then #  5 - MD run
   MODE='reset_counters'
+  TITLE='Copper: MD'
   NSTEP=100
   IPRINT=1
   ISAVE=10
-  TIMESTEP=8
+  TIMESTEP=8.0d0
   NDR=60
   NDW=70
   ELECTRONDYNAMICS='verlet'
   EMASS=500.0
-  ORTHOMAX=100
+  ORTHOMAX=300
   EMCUTOFF=3.0
-  ORTHOMAX=100
   IONDYNAMICS='verlet'
 else
   echo "ERROR: Run number not valid."
@@ -197,6 +201,7 @@ awk -v title="$TITLE" -v calculation="$CALCULATION" \
     sub(/IONVELOCITY/, ionvelocity); sub(/IONDAMPING/, iondamping);
     sub(/TEMPW/, tempw); sub(/FNOSEP/, fnosep);
      print}' cu-input.sample | sed '/NULL/d' > cu.in
- srun cp.x -input cu.in > log-cu
+    cat cu.cif >> cu.in 
+ #srun cp.x -input cu.in > log-cu
 
 done
